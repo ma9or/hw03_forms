0a9aac927c99fd6ebe3c6fcc8804b07d298c4021
Ильдар Ахметжанов <il.akhmetzhanov@yandex.kz>
2022-01-11 23:51:22 +0600

    Спринт №4 попытка №1

---
+++ yatube/about/views.py
@@ -0,0 +1,15 @@
+# from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Create your views here.
+
+
+class AboutAuthorView(TemplateView):
+    # В переменной template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница
+    template_name = 'about/author.html'
+
+
+class AboutTechView(TemplateView):
+    # В переменной template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница
+    template_name = 'about/tech.html'
